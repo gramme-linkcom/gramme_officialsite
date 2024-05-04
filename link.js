858c83962db279fdfe110499afc5fef6fdf1b8a4
ぐらむ/gramme <127509194+gramme-linkcom@users.noreply.github.com>
2024-05-04 21:58:29 +0900

    Add files via upload

--- link.js
+++ link.js
@@ -2,8 +2,8 @@ window.onload = onLoad;
 
 function onLoad() {
     link = document.getElementById('base');
-    let root = "gramme-linkcom.github.io"
-    link.setAttribute('href', "http://"+root);
+    let root = "gramme.f5.si"
+    link.setAttribute('href', "https://"+root);
 
     console.log("completed rewrite")
 }
